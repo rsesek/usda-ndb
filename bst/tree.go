4626239a6909fa3fb0689c5b360f50fdcb734e04
Robert Sesek <rsesek@google.com>
2013-04-08 17:17:07 -0400

    Implement an untested find and insert for Tree

--- bst/tree.go
+++ bst/tree.go
@@ -16,9 +16,9 @@
 //
 
 // Package bst implements a Binary Search Tree. The tree node values are actually
-// 2-tuples (search-key, app-data), with the search-key being the value that all
-// values in a node have in common. The app-data is used to refer back to some document
-// that is being searched for using the tree.
+// 2-tuples (search-key, app-token), with the search-key being the value that all
+// values in a node have in common. The app-token is used to refer back to some
+// object that is being searched for using the tree.
 package bst
 
 type Tree struct {
@@ -30,6 +30,7 @@ func NewTree() *Tree {
 }
 
 func (t *Tree) Find(value string) []string {
+	node := t.findNode(value, t.root)
 	return nil
 }
 
@@ -37,11 +38,35 @@ func (t *Tree) findNode(value string, node *Node) *Node {
 	if node == nil || node.value == value {
 		return node
 	}
-	return nil
+
+	if value < node.value {
+		return t.findNode(value, node.left)
+	}
+	return t.findNode(value, node.right)
 }
 
 func (t *Tree) Insert(p Pair) {
 	if t.root == nil {
 		t.root = MakeNode(p)
+	} else {
+		t.insertOn(p, t.root)
+	}
+}
+
+func (t *Tree) insertOn(p Pair, n *Node) {
+	if p.Value == n.value {
+		n.InsertPair(p)
+	} else if p.Value < n.value {
+		if n.left == nil {
+			n.left = MakeNode(p)
+		} else {
+			t.insertOn(p, n.left)
+		}
+	} else {
+		if n.right == nil {
+			n.right = MakeNode(p)
+		} else {
+			t.insertOn(p, n.right)
+		}
 	}
 }
