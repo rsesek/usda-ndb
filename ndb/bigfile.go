dff0a7487a8f6ec844d56af9c5231414e8e461a2
Robert Sesek <rsesek@google.com>
2013-04-08 23:19:52 -0400

    Create bigfile, a way to process the large ASCII dumps quickly line-by-line.

---
+++ ndb/bigfile.go
@@ -0,0 +1,119 @@
+//
+// USDA-NDB Viewer
+// Copyright 2013 Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+package ndb
+
+import (
+	"io"
+	"os"
+)
+
+// The number of bytes of a file to read, aligned to the nearest newline.
+const kAlignment = 4096
+
+type LineProcessor func(line string) error
+
+type bigFile struct {
+	processor LineProcessor // The function that accepts lines.
+	done      chan error    // Channel that workers spawned from readFile use to communicate.
+	workers   int           // Number of workers spawned from readFile.
+}
+
+// ReadFile reads the file at path |file| and processes lines using the |processor|
+// function. The processor can execute concurrently. Returns the first error that
+// occurs. The processor should communicate over a chanel to its own storage facility.
+func ReadFile(file string, processor LineProcessor) error {
+	bf := &bigFile{
+		processor: processor,
+		done:      make(chan error),
+	}
+
+	err := bf.readFile(file)
+	if err != nil {
+		return err
+	}
+
+	// Wait for the workers to complete.
+	for i := bf.workers; i > 0; i-- {
+		err := <-bf.done
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// readFile synchronously (from ReadFile) opens and reads the file into chunks. Each
+// chunk is then passed to a goroutine, which processes each line of the chunk individually.
+func (bf *bigFile) readFile(file string) error {
+	f, err := os.Open(file)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	buf := make([]byte, kAlignment)
+	for {
+		n, err := f.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				return nil
+			}
+			return err
+		}
+
+		// Walk the buffer back to the previous newline.
+		nOrig := n
+		for ; buf[n-1] != '\n'; n-- {
+		}
+
+		// If we moved at all, seek the file backwards.
+		if delta := n - nOrig; delta > 0 {
+			_, err := f.Seek(int64(delta), 1)
+			if err != nil {
+				return err
+			}
+		}
+
+		// Proceess the chunk.
+		bf.workers++
+		go bf.processChunk(string(buf[:n]))
+	}
+
+	return nil
+}
+
+// processChunk splits a chunk on \r\n and sends each whitespace-trimmed line to
+// the processor. Any errors from the processor are sent over the error channel,
+// or nil is sent when processing is done.
+func (bf *bigFile) processChunk(chunk string) {
+	var start int
+	for i := 0; i < len(chunk); i++ {
+		if chunk[i] == '\r' {
+			if err := bf.processor(chunk[start:i]); err != nil {
+				bf.done <- err
+				return
+			}
+			start = i + 1
+		} else if chunk[i] == '\n' {
+			start = i + 1
+		}
+	}
+
+	bf.done <- nil
+}
